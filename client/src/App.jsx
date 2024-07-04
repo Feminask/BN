920232a2f7cb9ccbc7b8f59daaa682b999b64cd4
Feminask <fmnzrasheed@gmail.com>
2024-07-04 20:47:29 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import MyPosts from './pages/MyPosts'
+import Signin from './pages/Signin'
 
 function App() {
   return (
-    <div className='text-3xl text-red-50'>
-      APP
-    </div>
+    <BrowserRouter>
+<Routes>
+<Route path='/' element={<Home></Home>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      <Route path='/sign-in' element={<Signin></Signin>}></Route>
+      <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
+
+      <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+         <Route path='/my-posts' element={<MyPosts></MyPosts>}></Route>
+
+
+
+</Routes>
+
+    </BrowserRouter>
   )
 }
 
